ed0a715c60be369a272fd6883a29f499b07e5f56
Daniel Kristensen <DKristensen88@gmail.com>
2025-11-10 10:47:31 +0100

    nye filer

---
+++ config.py
@@ -0,0 +1,14 @@
+"""
+
+This file contains configuration parameters for the Deep learning model.
+
+"""
+BATCH_SIZE = 0
+EPOCHS = 0
+LEARNING_RATE = 0
+# ... Andre parametre 
+
+S = 10 # Grid size
+B = 2 # Number of bounding boxes per grid cell
+C = 20 # Number of classes
+
