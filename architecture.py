ddff47f2938fc0293d40b0c132060b2f82057f5c
Daniel-Kristensen1 <dkristensen88@gmail.com>
2025-11-11 13:29:11 +0100

    opdateret model - conn layer

--- architecture.py
+++ architecture.py
@@ -1,53 +1,106 @@
-
-import os
-import random
-from pathlib import Path
-from typing import Tuple
-
-import numpy as np
-from PIL import Image
 import torch
 import torch.nn as nn
-import torch.optim as optim
-from torch.utils.data import Dataset, DataLoader, random_split
-import torchvision.transforms as T
+import torch.nn.functional as f
+import config
 
-#Ovenstående imports er fra ChatGPT. Aner ikke hvilke jeg skal bruge... Det skal sorteres senere.
+# Større ændring vi skal have styr på, "Hvorfor vi bruger den padding vi bruger"
 
-class YoloCNN(nn.Module):
-    def __init__(self):
+
+class SimpleCNN(nn.Module):
+    def __init__(self, num_classes: int=13, in_channels: int = 3): # Vi skal have ændret num_classes senere. in_channels = 3 er fint, da vi kører RGB farver.
         super().__init__()
-        # Conv. layer 1
-        self.conv1 = nn.Conv2d(in_channels=3, out_channels=64, kernel_size=7, stride=2, padding="same") #Måske kan pytorch håndtere "same" padding selv?
-        # Conv. layer 2
-        self.conv2 = nn.Conv2d(in_channels=64, out_channels=192, kernel_size=3, stride=1, padding="same")
-        # Conv. layer 3
-        self.conv3_1 = nn.Conv2d(in_channels=192/2, out_channels=128, kernel_size=1, stride=1, padding="same")
-        self.conv3_2 = nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, stride=1, padding="same")
-        self.conv3_3 = nn.Conv2d(in_channels=256, out_channels=256, kernel_size=1, stride=1, padding="same")
-        self.conv3_4 = nn.Conv2d(in_channels=256, out_channels=512, kernel_size=3, stride=1, padding="same")
-        # Conv. layer 4
-        self.conv4_1 = nn.Conv2d(in_channels=512/2, out_channels=256, kernel_size=1, stride=1, padding="same") #Dette lag er tilpasset maxpool der kommer fra tideligere lag. Ellers skal in channel være 512
-        self.conv4_2 = nn.Conv2d(in_channels=256, out_channels=512, kernel_size=1, stride=1, padding="same")
-        
-        self.conv4_3 = nn.Conv2d(in_channels=512, out_channels=256, kernel_size=1, stride=1, padding="same")
-        self.conv4_4 = nn.Conv2d(in_channels=256, out_channels=512, kernel_size=1, stride=1, padding="same")
-        
-        self.conv4_5 = nn.Conv2d(in_channels=512, out_channels=256, kernel_size=1, stride=1, padding="same")
-        self.conv4_6 = nn.Conv2d(in_channels=256, out_channels=512, kernel_size=1, stride=1, padding="same")
-        
-        self.conv4_7 = nn.Conv2d(in_channels=512, out_channels=256, kernel_size=1, stride=1, padding="same")
-        self.conv4_8 = nn.Conv2d(in_channels=256, out_channels=512, kernel_size=1, stride=1, padding="same")
-        #--------        
-        self.conv4_9 = nn.Conv2d(in_channels=512, out_channels=512, kernel_size=1, stride=1, padding="same")
-        self.conv4_10 = nn.Conv2d(in_channels=512, out_channels=1024, kernel_size=3, stride=1, padding="same")
-        
-        # Conv. layer 5
-        # Conv. layer 6
-        # Fully connected layer 1
-        # Fully connected layer 2
-        
+        ### conv1 ###
+        self.conv1 = nn.Conv2d(in_channels, out_channels=64, kernel_size=7, stride=2, padding=3) # Vi har 64 filtre, 7x7 kernel, 1 stride
+        self.act1 = nn.LeakyReLU(0.1)
+        self.pool1 = nn.MaxPool2d(2, 2) # Det første 2 tal er vores 2x2 kernel andet 2 tal er stride = 2
 
+        ### Conv2 ###
+        self.conv2 = nn.Conv2d(in_channels=64, out_channels=192, kernel_size=3, stride=1, padding=1)
+        self.act2 = nn.LeakyReLU(0.1)
+        self.pool2 = nn.MaxPool2d(2, 2)
+
+        ### Conv3 ###
+        self.conv3 = nn.Sequential(
+            nn.Conv2d(192, 128, kernel_size=1, stride=1), nn.LeakyReLU(0.1),
+            nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1), nn.LeakyReLU(0.1),
+            nn.Conv2d(256, 256, kernel_size=1, stride=1), nn.LeakyReLU(0.1),
+            nn.Conv2d(256, 512, kernel_size=3, stride=1, padding=1), nn.LeakyReLU(0.1),
+            nn.MaxPool2d(2, 2)
+            #Max pooling på kernel 2x2 og stride = 2
+        )
+
+        ### Conv4 ###
+        self.conv4 = nn.Sequential(
+            nn.Conv2d(512, 256, kernel_size=1, stride=1), nn.LeakyReLU(0.1),
+            nn.Conv2d(256, 512, kernel_size=3, stride=1, padding=1), nn.LeakyReLU(0.1),
+            #Gentag ovenstående 3 yderligere gange
+            nn.Conv2d(512, 256, kernel_size=1, stride=1), nn.LeakyReLU(0.1),
+            nn.Conv2d(256, 512, kernel_size=3, stride=1, padding=1), nn.LeakyReLU(0.1),
+            nn.Conv2d(512, 256, kernel_size=1, stride=1), nn.LeakyReLU(0.1),
+            nn.Conv2d(256, 512, kernel_size=3, stride=1, padding=1), nn.LeakyReLU(0.1),
+            nn.Conv2d(512, 256, kernel_size=1, stride=1), nn.LeakyReLU(0.1),
+            nn.Conv2d(256, 512, kernel_size=3, stride=1, padding=1), nn.LeakyReLU(0.1),
+
+
+            nn.Conv2d(512, 512, kernel_size=1, stride=1), nn.LeakyReLU(0.1),
+            nn.Conv2d(512, 1024, kernel_size=3, stride=1, padding=1), nn.LeakyReLU(0.1),
+            nn.MaxPool2d(2, 2)
+        )
+
+        ### Conv5 ###
+        self.conv5 = nn.Sequential(
+            nn.Conv2d(1024, 512, kernel_size=1, stride=1),nn.LeakyReLU(0.1),
+            nn.Conv2d(512, 1024, kernel_size=3, stride=1, padding=1), nn.LeakyReLU(0.1),
+            #Gentag ovenstående 1 yderligere gang
+            nn.Conv2d(1024, 512, kernel_size=1, stride=1), nn.LeakyReLU(0.1),
+            nn.Conv2d(512, 1024, kernel_size=3, stride=1, padding=1), nn.LeakyReLU(0.1),
+
+            nn.Conv2d(1024, 1024, kernel_size=3, stride=1, padding=1), nn.LeakyReLU(0.1),
+            nn.Conv2d(1024, 1024, kernel_size=3, stride=1, padding=1), nn.LeakyReLU(0.1),
+        )
+
+        ### Conv6 ###
+        self.conv6 = nn.Sequential(
+            nn.Conv2d(1024, 1024, kernel_size=3, stride=1, padding=1), nn.LeakyReLU(0.1),
+            nn.Conv2d(1024, 1024, kernel_size=3, stride=1, padding=1), nn.LeakyReLU(0.1),
+        )
+        
+        # Fully connected:
+
+        self.fc = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(config.S * config.S * 1024 ,4096),
+            # nn.dropout?
+            nn.LeakyReLU(0.1),
+            nn.Linear(4096, config.S * config.S * (config.B*5 + config.C))
+        )
 
     def forward(self, x):
+        # Aktiverer conv1 laget
+        x = self.pool1(self.act1(self.conv1(x)))
+        # Aktiverer conv2 laget
+        x = self.pool2(self.act2(self.conv2(x)))
+
+        # Aktiverer conv 3,4,5,6 lagende
+        x = self.conv3(x)
+        x = self.conv4(x)
+        x = self.conv5(x)
+        x = self.conv6(x)
+
+        # Fully connected 
+
+        x = self.fc(x)
         return x
+    
+
+model = SimpleCNN(num_classes=13)
+x = torch.randn(1, 3, 448, 448)
+
+with torch.no_grad():
+    y = model.pool1(model.act1(model.conv1(x)))
+    y = model.pool2(model.act2(model.conv2(y)))
+    y = model.conv3(y)
+    y = model.conv4(y)
+    y = model.conv5(y)  # <- nu uden stride=2
+    y = model.conv6(y)
+    print("Feature map efter conv6:", y.shape)  # forvent: (1, 1024, 14, 14)
