097ee02525f0d4662e9c9d3bc0e416c5fc4c0c04
Daniel-Kristensen1 <dkristensen88@gmail.com>
2025-11-11 19:30:27 +0100

    udkast til loss

--- loss.py
+++ loss.py
@@ -12,26 +12,44 @@ class LossFunc(nn.Module):
         self.lambda_noobj = config.NOOBJ
 
     def forward(self, pred, gt):
+        # Attributes
         pred_x, pred_y, pred_w, pred_h, pred_c = get_bb_attributes(pred)
         gt_x, gt_y, gt_w, gt_h, gt_c = get_bb_attributes(gt)
-        obj_mask = gt_c > 0
+        # Classes
+        pred_class_probs = get_bb_classes(pred) 
+        gt_class = get_bb_classes(gt)
+        # Masks
+        obj_ij = gt_c > 0 # 1 if object
+        obj_ij=obj_ij.float()
+        obj_i = obj_ij[..., None] # need to rethink???
+
+        noobj_ij = gt_c == 0 # 1 of no object
+        noobj_ij = noobj_ij.float() 
+        
         epsilon = 1e-6
 
 
 
         # Coordinate loss
-        loss_xy = self.lambda_coord * (((pred_x - gt_x) ** 2 + (pred_y - gt_y) ** 2) * obj_mask).sum()
-        loss_wh = self.lambda_coord * (((
+        loss_xy = self.lambda_coord * (obj_ij*((pred_x - gt_x) ** 2 + (pred_y - gt_y) ** 2)).sum()
+        loss_wh = self.lambda_coord * (obj_ij * ((
             torch.sqrt(pred_w + epsilon) - 
             torch.sqrt(gt_w + epsilon))**2 +
             (torch.sqrt(pred_h + epsilon) - 
-            torch.sqrt(gt_h + epsilon))**2) * 
-            obj_mask
-).sum()
+            torch.sqrt(gt_h + epsilon))**2)).sum()
+        
+        coord_loss = loss_xy+loss_wh
         # Confidence Loss
+        
+        conf_obj = (obj_ij * (pred_c-gt_c)**2).sum()
+        conf_noobj = self.lambda_noobj * (noobj_ij * ((pred_c-gt_c)**2)).sum() 
+        conf_loss = conf_obj+conf_noobj
 
         # Classification Loss
+        class_loss = (obj_i * (pred_class_probs-gt_class)**2).sum() # Usikker på om den håndtere klasserne korrekt
 
+
+        loss = coord_loss + conf_loss + class_loss # har ikke brugt IOU endnu??
 
 
 
@@ -43,4 +61,7 @@ def get_bb_attributes(data):
     h = data_attributes[..., 3::5]
     c = data_attributes[..., 4::5]
     return x, y, w, h, c
-     
+
+def get_bb_classes(data):
+    class_probs = data[..., config.B*5:]
+    return class_probs
