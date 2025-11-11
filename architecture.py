97663b6f682d99fd6e5f8a19bdacf4f7ac6da6b0
Trane98 <alextrendholm@hotmail.com>
2025-11-11 13:56:01 +0100

    Ændringer i aktiveringsfunktionerne

--- architecture.py
+++ architecture.py
@@ -11,58 +11,58 @@ class SimpleCNN(nn.Module):
         super().__init__()
         ### conv1 ###
         self.conv1 = nn.Conv2d(in_channels, out_channels=64, kernel_size=7, stride=2, padding=3) # Vi har 64 filtre, 7x7 kernel, 1 stride
-        self.act1 = nn.LeakyReLU(0.1)
+        self.act1 = nn.LeakyReLU(0.1, inplace=True)
         self.pool1 = nn.MaxPool2d(2, 2) # Det første 2 tal er vores 2x2 kernel andet 2 tal er stride = 2
 
         ### Conv2 ###
         self.conv2 = nn.Conv2d(in_channels=64, out_channels=192, kernel_size=3, stride=1, padding=1)
-        self.act2 = nn.LeakyReLU(0.1)
+        self.act2 = nn.LeakyReLU(0.1, inplace=True)
         self.pool2 = nn.MaxPool2d(2, 2)
 
         ### Conv3 ###
         self.conv3 = nn.Sequential(
-            nn.Conv2d(192, 128, kernel_size=1, stride=1), nn.LeakyReLU(0.1),
-            nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1), nn.LeakyReLU(0.1),
-            nn.Conv2d(256, 256, kernel_size=1, stride=1), nn.LeakyReLU(0.1),
-            nn.Conv2d(256, 512, kernel_size=3, stride=1, padding=1), nn.LeakyReLU(0.1),
+            nn.Conv2d(192, 128, kernel_size=1, stride=1), nn.LeakyReLU(0.1, inplace=True),
+            nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1), nn.LeakyReLU(0.1, inplace=True),
+            nn.Conv2d(256, 256, kernel_size=1, stride=1), nn.LeakyReLU(0.1, inplace=True),
+            nn.Conv2d(256, 512, kernel_size=3, stride=1, padding=1), nn.LeakyReLU(0.1, inplace=True),
             nn.MaxPool2d(2, 2)
             #Max pooling på kernel 2x2 og stride = 2
         )
 
         ### Conv4 ###
         self.conv4 = nn.Sequential(
-            nn.Conv2d(512, 256, kernel_size=1, stride=1), nn.LeakyReLU(0.1),
-            nn.Conv2d(256, 512, kernel_size=3, stride=1, padding=1), nn.LeakyReLU(0.1),
+            nn.Conv2d(512, 256, kernel_size=1, stride=1), nn.LeakyReLU(0.1, inplace=True),
+            nn.Conv2d(256, 512, kernel_size=3, stride=1, padding=1), nn.LeakyReLU(0.1, inplace=True),
             #Gentag ovenstående 3 yderligere gange
-            nn.Conv2d(512, 256, kernel_size=1, stride=1), nn.LeakyReLU(0.1),
-            nn.Conv2d(256, 512, kernel_size=3, stride=1, padding=1), nn.LeakyReLU(0.1),
-            nn.Conv2d(512, 256, kernel_size=1, stride=1), nn.LeakyReLU(0.1),
-            nn.Conv2d(256, 512, kernel_size=3, stride=1, padding=1), nn.LeakyReLU(0.1),
-            nn.Conv2d(512, 256, kernel_size=1, stride=1), nn.LeakyReLU(0.1),
-            nn.Conv2d(256, 512, kernel_size=3, stride=1, padding=1), nn.LeakyReLU(0.1),
+            nn.Conv2d(512, 256, kernel_size=1, stride=1), nn.LeakyReLU(0.1, inplace=True),
+            nn.Conv2d(256, 512, kernel_size=3, stride=1, padding=1), nn.LeakyReLU(0.1, inplace=True),
+            nn.Conv2d(512, 256, kernel_size=1, stride=1), nn.LeakyReLU(0.1, inplace=True),
+            nn.Conv2d(256, 512, kernel_size=3, stride=1, padding=1), nn.LeakyReLU(0.1, inplace=True),
+            nn.Conv2d(512, 256, kernel_size=1, stride=1), nn.LeakyReLU(0.1, inplace=True),
+            nn.Conv2d(256, 512, kernel_size=3, stride=1, padding=1), nn.LeakyReLU(0.1, inplace=True),
 
 
-            nn.Conv2d(512, 512, kernel_size=1, stride=1), nn.LeakyReLU(0.1),
-            nn.Conv2d(512, 1024, kernel_size=3, stride=1, padding=1), nn.LeakyReLU(0.1),
+            nn.Conv2d(512, 512, kernel_size=1, stride=1), nn.LeakyReLU(0.1, inplace=True),
+            nn.Conv2d(512, 1024, kernel_size=3, stride=1, padding=1), nn.LeakyReLU(0.1, inplace=True),
             nn.MaxPool2d(2, 2)
         )
 
         ### Conv5 ###
         self.conv5 = nn.Sequential(
-            nn.Conv2d(1024, 512, kernel_size=1, stride=1),nn.LeakyReLU(0.1),
-            nn.Conv2d(512, 1024, kernel_size=3, stride=1, padding=1), nn.LeakyReLU(0.1),
+            nn.Conv2d(1024, 512, kernel_size=1, stride=1),nn.LeakyReLU(0.1, inplace=True),
+            nn.Conv2d(512, 1024, kernel_size=3, stride=1, padding=1), nn.LeakyReLU(0.1, inplace=True),
             #Gentag ovenstående 1 yderligere gang
-            nn.Conv2d(1024, 512, kernel_size=1, stride=1), nn.LeakyReLU(0.1),
-            nn.Conv2d(512, 1024, kernel_size=3, stride=1, padding=1), nn.LeakyReLU(0.1),
+            nn.Conv2d(1024, 512, kernel_size=1, stride=1), nn.LeakyReLU(0.1, inplace=True),
+            nn.Conv2d(512, 1024, kernel_size=3, stride=1, padding=1), nn.LeakyReLU(0.1, inplace=True),
 
-            nn.Conv2d(1024, 1024, kernel_size=3, stride=1, padding=1), nn.LeakyReLU(0.1),
-            nn.Conv2d(1024, 1024, kernel_size=3, stride=1, padding=1), nn.LeakyReLU(0.1),
+            nn.Conv2d(1024, 1024, kernel_size=3, stride=1, padding=1), nn.LeakyReLU(0.1, inplace=True),
+            nn.Conv2d(1024, 1024, kernel_size=3, stride=1, padding=1), nn.LeakyReLU(0.1, inplace=True),
         )
 
         ### Conv6 ###
         self.conv6 = nn.Sequential(
-            nn.Conv2d(1024, 1024, kernel_size=3, stride=1, padding=1), nn.LeakyReLU(0.1),
-            nn.Conv2d(1024, 1024, kernel_size=3, stride=1, padding=1), nn.LeakyReLU(0.1),
+            nn.Conv2d(1024, 1024, kernel_size=3, stride=1, padding=1), nn.LeakyReLU(0.1, inplace=True),
+            nn.Conv2d(1024, 1024, kernel_size=3, stride=1, padding=1), nn.LeakyReLU(0.1, inplace=True),
         )
         
         # Fully connected:
