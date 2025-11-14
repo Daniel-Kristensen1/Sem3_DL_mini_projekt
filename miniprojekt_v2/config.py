265af0bc1292b1640ae6dd2563cc25f265583d86
Trane98 <alextrendholm@hotmail.com>
2025-11-14 11:00:30 +0100

    Opdatering til config

--- miniprojekt_v2/config.py
+++ miniprojekt_v2/config.py
@@ -9,13 +9,25 @@ from pathlib import Path
 
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-DATA_PATH = []
+# Datalokation
+TRAIN_JSON = Path(r"C:\Program Files (x86)\Programmering\3_semester\Mini_projekt2.0\data_splits_ready\train\train.json")
+TEST_JSON = Path(r"C:\Program Files (x86)\Programmering\3_semester\Mini_projekt2.0\data_splits_ready\test\test.json")
+VAL_JSON = Path(r"C:\Program Files (x86)\Programmering\3_semester\Mini_projekt2.0\data_splits_ready\val\val.json")
 
+TRAIN_IMAGES = Path(r"C:\Program Files (x86)\Programmering\3_semester\Mini_projekt2.0\data_splits_ready\train\images")
+TEST_IMAGES = Path(r"C:\Program Files (x86)\Programmering\3_semester\Mini_projekt2.0\data_splits_ready\test\images")
+VAL_IMAGES = Path(r"C:\Program Files (x86)\Programmering\3_semester\Mini_projekt2.0\data_splits_ready\val\images")
+
+
+# Klasserne vi har i datasættet i rigtig rækkefølge
 CLASSES = [
     "Adamant", "Clay", "Coal", "Copper", "Gold", "Iron",
     "Mined", "Mithril", "Motherload_ore", "Removable_ore",
     "Runeite", "Silver", "Tin",
 ]
+# Dictionary med klasserne og deres ID'er
+CLASSES_WITH_ID = {name: i + 1 for i, name in enumerate(CLASSES)}  # 0 reserveret til background
+
 NUM_CLASSES = len(CLASSES)
 
 BATCH_SIZE = 32
@@ -25,3 +37,5 @@ NUM_EPOCHS = 10
 LEARNING_RATE = 0.001
 
 LOSS_FN = []
+
+IMAGE_SIZE = (640, 640)
