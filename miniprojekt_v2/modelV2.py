e8da748735d8357ceeb6dec3ebf1d4bcbeda38d9
Daniel-Kristensen1 <dkristensen88@gmail.com>
2025-11-17 10:46:35 +0100

    update

--- miniprojekt_v2/modelV2.py
+++ miniprojekt_v2/modelV2.py
@@ -27,6 +27,7 @@ class Backbone(nn.Module):
 
 
 def create_object_detector(num_classes=config.NUM_CLASSES):
+      print(f"- Creating Object Detector...")
       print(f"- Creating Anchor generator...")
       anchor_gen = AnchorGenerator(
         sizes=((32, 64, 128, 256, 512),),
